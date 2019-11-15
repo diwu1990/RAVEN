43fe3aa797da5c170f58e414655efad1444bfd3a
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2019-11-14 23:41:33 -0600

    rewrite test_appr_taylor_utils

--- pe/appr_taylor_utils.py
+++ pe/appr_taylor_utils.py
@@ -3,7 +3,8 @@ import torch
 class RoundingNoGrad(torch.autograd.Function):
     """
     RoundingNoGrad is a rounding operation which bypasses the input gradient to output directly.
-    Original round()/floor()/ceil() opertions have a gradient of 0 everywhere, which is not useful when doing approximate computing.
+    Original round()/floor()/ceil() opertions have a gradient of 0 everywhere, which is not useful 
+    when doing approximate computing.
     This is something like the straight-through estimator (STE) for quantization-aware training.
     """
     # Note that both forward and backward are @staticmethods
@@ -30,12 +31,22 @@ def Trunc(input, intwidth=7, fracwidth=8, rounding="floor"):
     Trunc is an operation to convert data to format (1, intwidth, fracwidth).
     """
     scale = 2**fracwidth
-    max_val = (2**(intwidth + fracwidth) - 1) / scale
-    min_val = 0 - (2**(intwidth + fracwidth)) / scale
-    return RoundingNoGrad.apply(input.mul(scale), rounding).div(scale).clamp(min_val, max_val)
+    max_val = (2**(intwidth + fracwidth) - 1)
+    min_val = 0 - (2**(intwidth + fracwidth))
+    return RoundingNoGrad.apply(input.mul(scale), rounding).clamp(min_val, max_val).div(scale)
     
     
-def Appr_Taylor(scale, const, var, coeff, power, sign, fxp=True, intwidth=7, fracwidth=8, rounding="floor", keepwidth=True):
+def Appr_Taylor(scale, 
+                const, 
+                var, 
+                coeff, 
+                power, 
+                sign, 
+                fxp=True, 
+                intwidth=7, 
+                fracwidth=8, 
+                rounding="floor", 
+                keepwidth=True):
     """
     Calculate the result of approximate Taylor series.
     The results is calculated as:
@@ -53,7 +64,12 @@ def Appr_Taylor(scale, const, var, coeff, power, sign, fxp=True, intwidth=7, fra
     (1 + "intwidth" + "fracwidth"), and "rounding" indicates the rounding mode.
     """
     
-    def flp_poly(scale, const, var, coeff, power, sign):
+    def flp_poly(scale, 
+                 const, 
+                 var, 
+                 coeff, 
+                 power, 
+                 sign):
         # initialization
         acc = torch.zeros_like(var).add(const)
         
@@ -63,7 +79,16 @@ def Appr_Taylor(scale, const, var, coeff, power, sign, fxp=True, intwidth=7, fra
             
         return acc.mul(scale)
     
-    def fxp_poly(scale, const, var, coeff, power, sign, intwidth=7, fracwidth=8, rounding="floor", keepwidth=True):
+    def fxp_poly(scale, 
+                 const, 
+                 var, 
+                 coeff, 
+                 power, 
+                 sign, 
+                 intwidth=7, 
+                 fracwidth=8, 
+                 rounding="floor", 
+                 keepwidth=True):
         # 1) For multiplication,
         # each input has the format of (1, intwidth, fracwidth),
         # the output has the format of (1, 2 * intwidth + 1, 2 * fracwidth)
@@ -161,3 +186,83 @@ def Appr_Taylor(scale, const, var, coeff, power, sign, fxp=True, intwidth=7, fra
     else:
         raise ValueError("fxp mode have to be of bool type.")
         
+
+def exp_data_gen(distribution="uniform"):
+    # This is a function to generate data for chosing parameter for exp, which
+    # only cares about the data between 0 and 1, because data outside this can
+    # be scaled using LUT.
+    if distribution == "uniform":
+        data = torch.distributions.uniform.Uniform(torch.zeros([100000]), torch.ones([100000])).sample()
+    elif distribution == "middle":
+        mu = torch.ones([100000]).mul(0.5)
+        sigma = torch.ones([100000]).mul(0.3)
+        data = torch.distributions.normal.Normal(mu, sigma).sample()
+        data = data - data.floor()
+    elif distribution == "left":
+        mu = torch.ones([100000]).mul(0.25)
+        sigma = torch.ones([100000]).mul(0.3)
+        data = torch.distributions.normal.Normal(mu, sigma).sample()
+        data = data - data.floor()
+    elif distribution == "right":
+        mu = torch.ones([100000]).mul(0.75)
+        sigma = torch.ones([100000]).mul(0.3)
+        data = torch.distributions.normal.Normal(mu, sigma).sample()
+        data = data - data.floor()
+
+    return data
+
+
+def exp_param_gen(distribution="uniform", intwidth=7, fracwidth=8, rounding="round", keepwidth=True):
+    # define the floating-point input
+    data = exp_data_gen(distribution)
+
+    # reference model
+    ref_result = torch.exp(data)
+
+    # approximate taylor series
+    point_list = [0.0, 0.125, 0.250, 0.375, 0.500, 0.625, 0.750, 0.875]
+    for point in point_list:
+        scale = torch.exp(torch.Tensor([point]))
+        const = torch.Tensor([1.])
+        var   = data - var
+        coeff = [1/1]
+        power = [1]
+        sign  = [1]
+        min_err = []
+        max_err = []
+        rms_err = []
+
+        appr_result = Appr_Taylor(scale, 
+                                    const, 
+                                    var, 
+                                    coeff, 
+                                    power, 
+                                    sign, 
+                                    fxp=True, 
+                                    intwidth=7, 
+                                    fracwidth=8, 
+                                    rounding="floor", 
+                                    keepwidth=True)
+        appr_err = (appr_result - ref_result)/ref_result
+
+        min_err.append(appr_err.min().item())
+        max_err.append(appr_err.max().item())
+        rms_err.append(appr_err.mul(appr_err).mean().sqrt().item())
+
+        print(min_err)
+        print(max_err)
+        print(rms_err)
+
+        # # this loop is for number of terms
+        # for term_idx in range(7):
+        #     temp_coeff = coeff.append(coeff[-1])
+        #     # this loop is for shifting offset of coeff
+        #     for coeff_idx in range(4):
+        #         # this loop is for multiplication of var
+        #         for power_idx in range(2):
+        #             # this loop is for accumulation of terms
+        #             for sign_idx in range(2):
+
+
+
+
