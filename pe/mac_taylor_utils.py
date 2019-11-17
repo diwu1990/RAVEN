3b9077d4a6781ea4ff3f7dce36d6f69ab4639143
diwu1990 <wudidaizi@hotmail.com>
2019-11-16 22:30:26 -0600

    create mac_taylor_ops

--- pe/mac_taylor_utils.py
+++ pe/mac_taylor_utils.py
@@ -1,75 +1,30 @@
 import torch
+from RAVEN.pe.appr_taylor_utils import RoundingNoGrad, Trunc
 
-class RoundingNoGrad(torch.autograd.Function):
-    """
-    RoundingNoGrad is a rounding operation which bypasses the input gradient to output directly.
-    Original round()/floor()/ceil() opertions have a gradient of 0 everywhere, which is not useful 
-    when doing approximate computing.
-    This is something like the straight-through estimator (STE) for quantization-aware training.
-    """
-    # Note that both forward and backward are @staticmethods
-    @staticmethod
-    def forward(ctx, input, mode="round"):
-        if mode == "round":
-            return input.round()
-        elif mode == "floor":
-            return input.floor()
-        elif mode == "ceil":
-            return input.ceil()
-        else:
-            raise ValueError("Input rounding mode is not supported.")
-    
-    # This function has only a single output, so it gets only one gradient
-    @staticmethod
-    def backward(ctx, grad_output):
-        grad_input = grad_output
-        return grad_input, None
-    
-    
-def Trunc(input, intwidth=7, fracwidth=8, rounding="floor"):
-    """
-    Trunc is an operation to convert data to format (1, intwidth, fracwidth).
-    """
-    scale = 2**fracwidth
-    max_val = (2**(intwidth + fracwidth) - 1)
-    min_val = 0 - (2**(intwidth + fracwidth))
-    return RoundingNoGrad.apply(input.mul(scale), rounding).clamp(min_val, max_val).div(scale)
-    
-    
-def Appr_Taylor(scale, 
-                const, 
-                var, 
-                coeff, 
-                power, 
-                sign, 
-                fxp=True, 
-                intwidth=7, 
-                fracwidth=8, 
-                rounding="floor", 
-                keepwidth=True):
+def Mac_Taylor(scale, 
+               var, 
+               coeff, 
+               fxp=True, 
+               intwidth=7, 
+               fracwidth=8, 
+               rounding="floor", 
+               keepwidth=True):
     """
     Calculate the result of approximate Taylor series.
     The results is calculated as:
-    output = scale * (const + sum(coeff * var^power * sign))
+    output = scale * (const + sum(coeff * var^power))
     
-    The approximate Taylor series require following rules for hardware efficiency.
+    Assume that there are N taylor terms, which is the length of coeff, power always goes from 0 to N-1.
     1) Tensor scale is calculated by shifting input or using very small LUT.
-    2) Tensor const is calculated using LUT
-    3) Tensor var is calulated as (input - offset)
-    4) Power[i] == 1, when i == 0
-    5) Power[i] == either power[i-1] or power[i-1]+1, when i > 0
-    6) Coeff[i] == 2^k, where k is a positive/negtive integer.
+    2) Tensor var is calulated as (input - offset)
     
     "fxp" means whether to performance fixed point calculation, in which the data bitwidth can be expressed as
     (1 + "intwidth" + "fracwidth"), and "rounding" indicates the rounding mode.
     """
     
     def flp_poly(scale, 
-                 const, 
                  var, 
-                 coeff, 
-                 power, 
-                 sign):
+                 coeff):
         # initialization
         acc = torch.zeros_like(var).add(const)
         
@@ -112,17 +67,17 @@ def Appr_Taylor(scale,
                     intwidth = intwidth, 
                     fracwidth = fracwidth, 
                     rounding = rounding)
-
+        
         # const is of format (1, 2 * intwidth + 1, 2 * fracwidth), as it 
         # participates in the accumulation
         const = Trunc(const, 
                       intwidth = 2 * intwidth + 2, 
                       fracwidth = 2 * fracwidth, 
                       rounding = rounding)
-
+        
         # initialization
         acc = acc.add(const)
-
+        
         # calculate the first term with power value of 1
         shift = Trunc(prod.mul(coeff[0]), 
                       intwidth = intwidth, 
@@ -530,7 +485,6 @@ def log_param_gen(distribution="uniform", intwidth=7, fracwidth=8, rounding="rou
     
     # reference model
     ref_result = torch.log(data)
-    print(ref_result)
 
     # approximate taylor series
     point_list = [1.0]
