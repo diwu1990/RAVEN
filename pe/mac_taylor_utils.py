97d8b46419ae50e64f2cb2bf47111e9feb605e97
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2019-11-19 13:59:43 -0600

    add test data csv

--- pe/mac_taylor_utils.py
+++ pe/mac_taylor_utils.py
@@ -132,6 +132,22 @@ def MAC_Taylor(scale,
     
     
 def point_search(func="exp", uniform=True, fxp=True, intwidth=7, fracwidth=8, valid=True, rounding_coeff="round", rounding_var="round", keepwidth=True):
+    output_file = func + "_rms_error"
+    if fxp is True:
+        output_file = output_file + "_fxp"
+    else:
+        output_file = output_file + "_flp"
+        
+    if uniform is True:
+        output_file = output_file + "_uniform"
+    else:
+        output_file = output_file + "_guassian"
+        
+    output_file = output_file + ".csv"
+    
+    f = open(output_file, "w+")
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # choose data range according to function
     if func == "div":
         data_range = "0.5_1.0"
@@ -141,7 +157,7 @@ def point_search(func="exp", uniform=True, fxp=True, intwidth=7, fracwidth=8, va
         data_range = "0.0_1.0"
         # varying the Taylor expansion point
         # point_list = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-        point_list = [0.00, 0.25, 0.05, 0.75, 1.00]
+        point_list = [0.00, 0.25, 0.50, 0.75, 1.00]
         # varying the distribution of data
         mu_list = [0.25, 0.5, 0.75]
     if func == "log":
@@ -153,11 +169,15 @@ def point_search(func="exp", uniform=True, fxp=True, intwidth=7, fracwidth=8, va
     if uniform == True:
         sigma = 2
         mu_list = [0.5]
+        f.write("uniform, \n")
     elif uniform == False:
         sigma = 0.2
-        
+        f.write("guassian, \n")
+    
     for mu_value in mu_list:
-        data = data_gen(data_range=data_range, mu=mu_value, sigma=sigma).cuda()
+        f.write("mu, "+str(mu_value)+",\n")
+
+        data = data_gen(data_range=data_range, mu=mu_value, sigma=sigma).to(device)
         if func == "log":
             data = Trunc(data, 
                          intwidth=intwidth, 
@@ -176,6 +196,8 @@ def point_search(func="exp", uniform=True, fxp=True, intwidth=7, fracwidth=8, va
             ref_result = torch.log(data)
         
         for point in point_list:
+            f.write("point, "+str(point)+",\n")
+            
             coeff = [1/1, 1/1, 1/1, 1/1, 1/1, 1/1, 1/1, 1/1, 1/1, 1/1]
             if func == "exp":
                 coeff = [1/1, 1/1, 1/2, 1/6, 1/24, 1/120, 1/720, 1/5040, 1/40320, 1/362880]
@@ -195,13 +217,13 @@ def point_search(func="exp", uniform=True, fxp=True, intwidth=7, fracwidth=8, va
             for idx in range(2, len(coeff)):
                 temp_coeff = coeff[0:idx]
                 if func == "exp":
-                    temp_scale = torch.exp(torch.tensor([point])).cuda()
+                    temp_scale = torch.exp(torch.tensor([point])).to(device)
                     temp_var = data - point
                 elif func == "div":
-                    temp_scale = torch.tensor([point]).cuda()
+                    temp_scale = torch.tensor([point]).to(device)
                     temp_var = point - data
                 elif func == "log":
-                    temp_scale = torch.tensor([-1.]).cuda()
+                    temp_scale = torch.tensor([-1.]).to(device)
                     temp_var = point - data
                 
                 appr_result = MAC_Taylor(temp_scale, 
@@ -244,10 +266,17 @@ def point_search(func="exp", uniform=True, fxp=True, intwidth=7, fracwidth=8, va
                 max_err = max_err[0:valid_length]
                 avg_err = avg_err[0:valid_length]
                 rms_err = rms_err[0:valid_length]
-                
+            
+            for i in rms_err:
+                f.write(str(i.item())+", ")
+            
+            f.write("\n")
+            
             print("eff coeff:", ["{0:0.10f}".format(i) for i in eff_coeff])
             print("min error:", ["{0:0.10f}".format(i) for i in min_err])
             print("max error:", ["{0:0.10f}".format(i) for i in max_err])
             print("avg error:", ["{0:0.10f}".format(i) for i in avg_err])
             print("rms error:", ["{0:0.10f}".format(i) for i in rms_err])
             print("")
+            
+        f.write("\n")
