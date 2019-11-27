3480250e96808cecf4ca1c2e31a9050e135cf2b9
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2019-11-27 09:53:22 -0600

    uno pe hw bug fix

--- hw/pe/uno.sv
+++ hw/pe/uno.sv
@@ -24,13 +24,14 @@ module uno (
 logic [`MAC_BW-1 : 0] x_norm;
 logic [3 : 0] x_int;
 logic [`MAC_BW-1 : 0] exp_scale;
+logic [`MAC_BW-1 : 0] point;
 logic [3 : 0] shiftx;
 logic [2*`MAC_BW-1 : 0] log_offset;
 
 assign x_int = X[11 : 8];
 assign exp_int = X[11 : 8];
 
-assign x_norm = x << shiftx;
+assign x_norm = X << shiftx;
 
 assign point_sub_x = point - x_norm;
 
@@ -45,19 +46,19 @@ priority_enc_12 U_priority_enc_12(.in(X),
 
 assign scale  = (op == 2'b01) ? Y >> shiftx : //div; y*2^(-ex)
                 (op == 2'b10) ? exp_scale : // exp: exp(x_int)
-                (op == 2'b11) ? {MAC_BW{1'b1}} : {MAC_BW{1'b0}}; // log: -1
+                (op == 2'b11) ? {`MAC_BW{1'b1}} : {`MAC_BW{1'b0}}; // log: -1
 
-assign offset = (op == 2'b01) ? {MAC_BW{1'b0}} : // div: 0
-                (op == 2'b10) ? {MAC_BW{1'b0}} : // exp: 0
-                (op == 2'b11) ? log_offset : {MAC_BW{1'b0}}; // log: log(2^(ex))
+assign offset = (op == 2'b01) ? {`MAC_BW{1'b0}} : // div: 0
+                (op == 2'b10) ? {`MAC_BW{1'b0}} : // exp: 0
+                (op == 2'b11) ? log_offset : {`MAC_BW{1'b0}}; // log: log(2^(ex))
 
-assign point  = (op == 2'b01) ? 12'b000011000000  : // div: 0.75
-                (op == 2'b10) ? 12'b000000000000  : // exp: 0
-                (op == 2'b11) ? 12'b000011000000 : {MAC_BW{1'b0}} ; // log: 0.75
+assign point  = (op == 2'b01) ? 12'b000011000000 : // div: 0.75
+                (op == 2'b10) ? 12'b000000000000 : // exp: 0
+                (op == 2'b11) ? 12'b000011000000 : {`MAC_BW{1'b0}} ; // log: 0.75
 
-assign var    = (op == 2'b01) ? point_sub_x  : // div: POINT - x_norm
+assign var    = (op == 2'b01) ? point_sub_x : // div: POINT - x_norm
                 (op == 2'b10) ? {4{x[11]}, x_frac[7:0]}  : // exp: X_frac
-                (op == 2'b11) ? point_sub_x : {MAC_BW{1'b0}} ; // log: POINT - x_norm  
+                (op == 2'b11) ? point_sub_x : {`MAC_BW{1'b0}} ; // log: POINT - x_norm  
 
 mac U_mac(.clk(clk),
           .rst_n(rst_n),
