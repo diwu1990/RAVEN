87e9478c6ae8b562ce973be7a226483b0f418874
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2020-05-08 18:48:16 -0500

    update gen_trace_conv2d_os

--- trace_sim/sram/gen_trace_conv2d_os.py
+++ trace_sim/sram/gen_trace_conv2d_os.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from copy import deepcopy
 from tqdm import tqdm
 
 
@@ -66,53 +67,59 @@ def gen_trace_conv2d_os(
         for c in range(ochn):
             remained_nc.append(n * ochn + c)
 
-    act_cols = np.asarray([[x < min(len(remained_nc), mac_cols) for x in range(mac_cols)]], dtype=int)
-    remained_nc = np.array(remained_nc, dtype=float)
+    act_cols = np.array([[x < min(len(remained_nc), mac_cols) for x in range(mac_cols)]], dtype=int)
+    remained_nc = np.array(remained_nc, dtype=int)
     remained_nc = remained_nc[int(np.sum(act_cols)):]
 
-    act_rows = np.asarray([[x < min(ofm_size, mac_rows)] for x in range(mac_rows)], dtype=int)
+    act_rows = np.array([[x < min(ofm_size_chn, mac_rows)] for x in range(mac_rows)], dtype=int)
 
-    init_col_idx = np.zeros(mac_cols) \
-                 + np.array([c for c in range(np.sum(act_cols))] + [0 for c in range(mac_cols - np.sum(act_cols))], dtype=float)
-    i_col_ofm_n = np.floor(init_col_idx / ochn)
+    act_idx = np.zeros((mac_rows, mac_cols), dtype=bool)
+    for r in range(np.sum(act_rows)):
+        for c in range(np.sum(act_cols)):
+            act_idx[r, c] = True
+
+    init_col_idx = np.zeros(mac_cols, dtype=int) \
+                 + np.array([c for c in range(np.sum(act_cols))] + [0 for c in range(mac_cols - np.sum(act_cols))], dtype=int)
+    i_col_ofm_n = np.floor(init_col_idx / ochn).astype("int")
+
     # the ofm channel index that each column of pes is assigned for computing
     i_col_ofm_c = init_col_idx % ochn
     # the ofm h index that each pe is assigned for computing
-    i_row_ofm_h = np.zeros((mac_rows, mac_cols))
+    i_row_ofm_h = np.zeros((mac_rows, mac_cols), dtype=int)
     # the ofm w index that each pe is assigned for computing
-    i_row_ofm_w = np.zeros((mac_rows, mac_cols)) \
-                + np.array([[r] for r in range(np.sum(act_rows))] + [[0] for c in range(mac_rows - np.sum(act_rows))], dtype=float)
+    i_row_ofm_w = np.zeros((mac_rows, mac_cols), dtype=int) \
+                + np.array([[r] for r in range(np.sum(act_rows))] + [[0] for c in range(mac_rows - np.sum(act_rows))], dtype=int)
 
     # the weight input channel index that each pe is assigned for computing
-    i_row_wgt_ci = np.zeros((mac_rows, mac_cols))
+    i_row_wgt_ci = np.zeros((mac_rows, mac_cols), dtype=int)
     for r in range(mac_rows):
         for c in range(mac_cols):
             i_row_wgt_ci[r, c] = 0 - r - c
 
     # the weight h index that each pe is assigned for computing
     # as width dimension is updated before height dimension, height is initialized to 0
-    i_row_wgt_h = np.zeros((mac_rows, mac_cols))
+    i_row_wgt_h = np.zeros((mac_rows, mac_cols), dtype=int)
     # the weight w index that each pe is assigned for computing
     # width dimension is used to determine whether a pe is ocuppied by a non-negative element
-    i_row_wgt_w = np.zeros((mac_rows, mac_cols))
+    i_row_wgt_w = np.zeros((mac_rows, mac_cols), dtype=int)
 
     # the ofm batch index that each pe is assigned for output
-    o_col_ofm_n = np.zeros(mac_cols)
+    o_col_ofm_n = np.zeros(mac_cols, dtype=int)
     # the ofm channel index that each column of pes is assigned for output
-    o_col_ofm_c = np.zeros(mac_cols)
+    o_col_ofm_c = np.zeros(mac_cols, dtype=int)
     # the ofm h index that each pe is assigned for output
-    o_row_ofm_h = np.zeros((mac_rows, mac_cols))
+    o_row_ofm_h = np.zeros((mac_rows, mac_cols), dtype=int)
     # the ofm w index that each pe is assigned for output
-    o_row_ofm_w = np.zeros((mac_rows, mac_cols))
+    o_row_ofm_w = np.zeros((mac_rows, mac_cols), dtype=int)
     # indicate which pixel to output
-    out_vld = np.zeros((mac_rows, mac_cols))
+    out_vld = np.zeros((mac_rows, mac_cols), dtype=bool)
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     # implementing different mapping strategies
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     
     def mapping_0(
-            remained_nc, act_cols, act_rows, \
+            remained_nc, act_idx, \
             i_col_ofm_n, i_col_ofm_c, i_row_ofm_h, i_row_ofm_w, \
             i_row_wgt_ci, i_row_wgt_h, i_row_wgt_w, \
             o_col_ofm_n, o_col_ofm_c, o_row_ofm_h, o_row_ofm_w
@@ -127,13 +134,10 @@ def gen_trace_conv2d_os(
         # this mapping function implements strategy 2
 
         # get the ofm index before update calculation
-        o_col_ofm_n = i_col_ofm_n
-        o_col_ofm_c = i_col_ofm_c
-        o_row_ofm_h = i_row_ofm_h
-        o_row_ofm_w = i_row_ofm_w
-
-        # get the index of active PEs
-        act_idx = np.matmul(act_rows, act_cols) != 0
+        o_col_ofm_n = deepcopy(i_col_ofm_n)
+        o_col_ofm_c = deepcopy(i_col_ofm_c)
+        o_row_ofm_h = deepcopy(i_row_ofm_h)
+        o_row_ofm_w = deepcopy(i_row_ofm_w)
 
         # processing one pixel
         # assume different channels are read most frequently, as it's NHWC for feature map
@@ -155,11 +159,6 @@ def gen_trace_conv2d_os(
 
         out_vld = i_row_wgt_h_carry
 
-        # print("i_row_wgt_h_carry\n", i_row_wgt_h_carry)
-        # print("i_row_ofm_w\n", i_row_ofm_w)
-        # print("i_row_ofm_w[i_row_wgt_h_carry]\n", i_row_ofm_w[i_row_wgt_h_carry])
-        # print("mac_rows\n", mac_rows)
-
         # processing next pixel in the same channel
         i_row_ofm_w[i_row_wgt_h_carry] += mac_rows
         # i_row_ofm_w_carry indicates that one ofm row is done
@@ -167,62 +166,62 @@ def gen_trace_conv2d_os(
         i_row_ofm_w[i_row_ofm_w_carry] = i_row_ofm_w[i_row_ofm_w_carry] % ofm_w
 
         i_row_ofm_h[i_row_ofm_w_carry] += 1
-
-        next_px = i_row_ofm_h * ofm_w \
-                   + i_row_ofm_w
-        diff_px = ofm_size_chn - next_px[:, 0] - 1 == 0
-        act_rows[ofm_size_chn - next_px[:, 0] - 1 < 0] = 0
-        for r in range(mac_rows-2, -1, -1):
-            if act_rows[r, 0] == 0 or diff_px[r] == 1:
-                act_rows[r+1, 0] = 0
-
         # i_row_ofm_h_carry indicates that one ofm channel is done
         i_row_ofm_h_carry = (np.floor(i_row_ofm_h / ofm_h) == 1) * act_idx
         i_row_ofm_h[i_row_ofm_h_carry] = 0
 
-        # print(np.sum(i_row_ofm_h_carry.astype("int")))
-        # pbar.update(np.sum(i_row_ofm_h_carry.astype("int")))
 
+        next_px = i_row_ofm_h * ofm_w \
+                   + i_row_ofm_w
+        diff_px = ofm_size_chn - next_px - 1 == 0
+        act_idx[ofm_size_chn - next_px - 1 < 0] = 0
+        for r in range(mac_rows-2, -1, -1):
+            for c in range(mac_rows-1, -1, -1):
+                if act_idx[r, c] == 0 or diff_px[r, c] == 1:
+                    act_idx[r+1, c] = False
+                    
         # processing next channel
-        top_sum = np.sum(i_row_ofm_h_carry[0, :].astype("float"))
-        print("i_row_ofm_h_carry[0, :]\n", i_row_ofm_h_carry[0, :])
+        # print("i_row_ofm_h_carry\n", i_row_ofm_h_carry)
+        # print("i_row_ofm_h_carry[0, :]\n", i_row_ofm_h_carry[0, :])
+        top_sum = np.sum(i_row_ofm_h_carry[0, :].astype("int"))
         assert top_sum <= 1, "sum of i_row_ofm_h_carry[0] should be no greater than 1."
+        # i_col_ofm_n[i_row_ofm_h_carry[0, :]] = np.floor(remained_nc[0] / ochn)
+        # i_col_ofm_c[i_row_ofm_h_carry[0, :]] = remained_nc[0] % ochn
         if len(remained_nc) > 0:
-            # if more remained channels, assign channel and update act_cols
-            i_col_ofm_n[i_row_ofm_h_carry[0, :]] = np.floor(remained_nc[0] / ochn)
-            i_col_ofm_c[i_row_ofm_h_carry[0, :]] = remained_nc[0] % ochn
             if top_sum == 1:
-                print("process a new channel")
-                act_rows[i_row_ofm_h_carry[0, :]] = 
+                # if more remained channels, assign channel and update act_cols
+                i_col_ofm_n[i_row_ofm_h_carry[0, :]] = int(np.floor(remained_nc[0] / ochn))
+                i_col_ofm_c[i_row_ofm_h_carry[0, :]] = int(remained_nc[0] % ochn)
+
+                act_rows_temp = min(ofm_size_chn, mac_rows)
+                for c in range(len(i_row_ofm_h_carry[0, :])):
+                    if i_row_ofm_h_carry[0, c] == True:
+                        new_cols = c
+                act_idx[0:act_rows_temp, new_cols] = True
+                act_idx[act_rows_temp:, new_cols] = False
+                i_row_ofm_h[:, new_cols] = 0
+                i_row_wgt_h[:, new_cols] = 0
+                i_row_wgt_w[:, new_cols] = 0
+
+                i_row_ofm_w[0:act_rows_temp, new_cols] = np.array([r for r in range(act_rows_temp)])
+                i_row_wgt_ci[0:act_rows_temp, new_cols] = np.array([-r for r in range(act_rows_temp)])
+                
                 remained_nc = remained_nc[1:]
-            else:
-                pass
-            # and all columns will be active
-            # act_cols = np.asarray([[x < mac_cols for x in range(mac_cols)]], dtype=int)
         else:
-            print("no remained_nc")
             # if no more remained channels, deactivate the finished column
-            act_cols[0][i_row_ofm_h_carry[0, :]] = 0
-            # print(act_cols)
-        print("i_col_ofm_n\n", i_col_ofm_n)
-        print("i_col_ofm_c\n", i_col_ofm_c)
-        print("act_cols\n", act_cols)
-        print("act_rows\n", act_rows)
+            act_idx[i_row_ofm_h_carry] = 0
 
-
-        return remained_nc, act_cols, act_rows, \
+        return remained_nc, act_idx, \
                 i_col_ofm_n, i_col_ofm_c, i_row_ofm_h, i_row_ofm_w, \
                 i_row_wgt_ci, i_row_wgt_h, i_row_wgt_w, \
                 o_col_ofm_n, o_col_ofm_c, o_row_ofm_h, o_row_ofm_w, out_vld
 
-    while cycles < 300:
-    # while not all_px_write_done:
+    while not all_px_write_done:
         print("cycles===============>", cycles)
         if fm_fmt is "NHWC":
             # all indexes start from 0, negative means not mapped.
             # print("remained_nc\n", remained_nc)
-            # print("act_cols\n", act_cols)
-            # print("act_rows\n", act_rows)
+            # print("act_idx\n", act_idx)
             # print("i_col_ofm_n\n", i_col_ofm_n)
             # print("i_col_ofm_c\n", i_col_ofm_c)
             # print("i_row_ofm_h\n", i_row_ofm_h)
@@ -250,7 +249,7 @@ def gen_trace_conv2d_os(
             # print("wgt_addr\n", wgt_addr)
             wgt_read = ""
             for c in range(mac_cols):
-                if i_row_wgt_ci[0, c] < 0:
+                if i_row_wgt_ci[0, c] < 0 and act_idx[0, c] == 1:
                     wgt_read += ", "
                 else:
                     wgt_read += str(int(wgt_addr[0, c] + wgt_base)) + ", "
@@ -290,23 +289,26 @@ def gen_trace_conv2d_os(
                      + o_row_ofm_w * ochn \
                      + o_col_ofm_c
             
-            # print("ofm_addr\n", ofm_addr)
-            # print("o_col_ofm_n\n", o_col_ofm_n)
-            # print("o_col_ofm_c\n", o_col_ofm_c)
-            # print("o_row_ofm_h\n", o_row_ofm_h)
-            # print("o_row_ofm_w\n", o_row_ofm_w)
+            print(out_vld)
             ofm_write = ""
-            for c in range(mac_cols):
-                col_valid = False
-                for r in range(mac_rows):
-                    if out_vld[r, c] is True:
-                        col_valid = True
-                        ofm_write += str(int(ofm_addr[r, c] + ofm_base)) + ", "
+            col_vld = np.sum(out_vld, axis=0)
+            ofm_vld = np.sum(col_vld)
+
+            if ofm_vld != 0:
+                for c in range(mac_cols):
+                    assert col_vld[c] <= 1, "One column should not output two ofm at the same cycle."
+                    if col_vld[c] == 1:
+                        for r in range(mac_rows):
+                            if out_vld[r, c] == 1:
+                                out_row = r
+                        ofm_write += str(int(ofm_addr[out_row, c] + ofm_base)) + ", "
                         ofm_write_cnt += 1
-                if not col_valid:
-                    ofm_write += ", "
+                    else:
+                        ofm_write += ", "
 
             # generate trace for read and write at each cycle
+            print("ifm_read_cnt", ifm_read_cnt)
+            print("total_read_cnt", total_read_cnt)
             if ifm_read_cnt >= total_read_cnt:
                 all_px_read_done = True
 
@@ -315,26 +317,28 @@ def gen_trace_conv2d_os(
                 read_outfile.write(read_trace)
                 read_cycles += 1
 
+            print("ofm_write_cnt", ofm_write_cnt)
+            print("total_write_cnt", total_write_cnt)
             if ofm_write_cnt >= total_write_cnt:
                 all_px_write_done = True
                 
-            if not all_px_write_done:
+            if not all_px_write_done and ofm_vld != 0:
                 write_trace = str(cycles) + ", " + ofm_write + "\n"
                 write_outfile.write(write_trace)
                 write_cycles += 1
 
             # calculate the percentage of PE utilization at each cycle
-            val_pe_count = np.sum((i_row_ofm_h > 0).astype('float'))
+            val_pe_count = np.sum((i_row_ofm_h > 0).astype('int'))
             utilization += val_pe_count/mac_rows/mac_cols
 
             cycles += 1
 
             # if not all ofm pixels are done, according to the border information of systolic array, generate the trace
-            remained_nc, act_cols, act_rows, \
+            remained_nc, act_idx, \
             i_col_ofm_n, i_col_ofm_c, i_row_ofm_h, i_row_ofm_w, \
             i_row_wgt_ci, i_row_wgt_h, i_row_wgt_w, \
             o_col_ofm_n, o_col_ofm_c, o_row_ofm_h, o_row_ofm_w, out_vld = mapping_0(
-                                                                            remained_nc, act_cols, act_rows, \
+                                                                            remained_nc, act_idx, \
                                                                             i_col_ofm_n, i_col_ofm_c, i_row_ofm_h, i_row_ofm_w, \
                                                                             i_row_wgt_ci, i_row_wgt_h, i_row_wgt_w, \
                                                                             o_col_ofm_n, o_col_ofm_c, o_row_ofm_h, o_row_ofm_w
