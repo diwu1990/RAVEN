ca23126086cee21559d2d0cb03db785253fa1038
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2020-09-20 11:54:41 -0500

    update mac_based sv

--- hw/simd_array_64/mac_based/demux.sv
+++ hw/simd_array_64/mac_based/demux.sv
@@ -6,8 +6,12 @@ module demux (
     input logic [`MAC_BW-1 : 0] iB [63 : 0],
     output logic [`MAC_BW-1 : 0] iA_mac [63 : 0],
     output logic [`MAC_BW-1 : 0] iB_mac [63 : 0],
-    output logic [`MAC_BW-1 : 0] iA_nl [63 : 0],
-    output logic [`MAC_BW-1 : 0] iB_nl [63 : 0]
+    output logic [`MAC_BW-1 : 0] iA_div [63 : 0],
+    output logic [`MAC_BW-1 : 0] iB_div [63 : 0],
+    output logic [`MAC_BW-1 : 0] iA_exp [63 : 0],
+    output logic [`MAC_BW-1 : 0] iB_exp [63 : 0],
+    output logic [`MAC_BW-1 : 0] iA_log [63 : 0],
+    output logic [`MAC_BW-1 : 0] iB_log [63 : 0],
 );
     
     genvar i;
@@ -16,8 +20,14 @@ module demux (
             assign iA_mac[i] = (mode == 0) ? iA[i] : 0;
             assign iB_mac[i] = (mode == 0) ? iB[i] : 0;
 
-            assign iA_nl[i] = (mode != 0) ? iA[i] : 0;
-            assign iB_nl[i] = (mode != 0) ? iB[i] : 0;
+            assign iA_div[i] = (mode == 1) ? iA[i] : 0;
+            assign iB_div[i] = (mode == 1) ? iB[i] : 0;
+
+            assign iA_exp[i] = (mode == 2) ? iA[i] : 0;
+            assign iB_exp[i] = (mode == 2) ? iB[i] : 0;
+
+            assign iA_log[i] = (mode == 3) ? iA[i] : 0;
+            assign iB_log[i] = (mode == 3) ? iB[i] : 0;
         end
     endgenerate
 
--- hw/simd_array_64/mac_based/mux.sv
+++ hw/simd_array_64/mac_based/mux.sv
@@ -3,14 +3,23 @@
 module mux (
     input logic [1:0] mode,
     input logic [`MAC_BW-1 : 0] oC_mac [63 : 0],
-    input logic [`MAC_BW-1 : 0] oC_nl [63 : 0],
+    input logic [`MAC_BW-1 : 0] oC_div [63 : 0],
+    input logic [`MAC_BW-1 : 0] oC_exp [63 : 0],
+    input logic [`MAC_BW-1 : 0] oC_log [63 : 0],
     output logic [`MAC_BW-1 : 0] oC [63 : 0]
 );
     
     genvar i;
     generate
         for (i = 0; i < 64; i++) begin
-            assign oC[i] = (mode == 0) ? oC_mac[i] : oC_nl[i];
+            always_comb begin : proc_oC
+                case (mode)
+                    2'd0: oC[i] = oC_mac[i];
+                    2'd1: oC[i] = oC_div[i];
+                    2'd2: oC[i] = oC_exp[i];
+                    2'd3: oC[i] = oC_log[i];
+                endcase
+            end
         end
     endgenerate
 
