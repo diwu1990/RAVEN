b4aecef9b1d5e0a195563c68575ac7340fb45f13
diwu1990 <diwu0111@hotmail.com>
2021-08-12 00:15:18 -0500

    update RAVEN pe test

---
+++ test/raven/test_raven_plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# cycle=4, bitwidth-reduce=False
+config1 = "4-4-F"
+div_acc = [0.0018, ]
+exp_acc = [0.0018, ]
+log_acc = [0.0018, ]
+
+# cycle=4, bitwidth-reduce=True
+config2 = "4-4-T"
+
+# cycle=8, bitwidth-reduce=False
+config3 = "8-8-F"
+
+# cycle=8, bitwidth-reduce=True
+config4 = "8-8-T"
