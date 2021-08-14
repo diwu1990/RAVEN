4a1325fee1af00aa40e0fdd5d1f91bbc0353df52
diwu1990 <diwu0111@hotmail.com>
2021-08-14 00:38:36 -0500

    update code

--- hw/raven/pe/offset_gen.sv
+++ hw/raven/pe/offset_gen.sv
@@ -35,39 +35,39 @@ module offset_gen #(
 
     always_ff @(*) begin : proc_log_offset
         case (shiftx)
-            5'b00000 : log_offset <= {ACC_BW}'b0001000000000000;
-            5'b00001 : log_offset <= {ACC_BW}'b0010101110000000;
-            5'b00010 : log_offset <= ACC_BW'b0111011001000000;
-            5'b00011 : log_offset <= ACC_BW'b0111111111110000;
-            5'b00100 : log_offset <= ACC_BW'b0111111111110000;
-            5'b00101 : log_offset <= ACC_BW'b0111111111110000;
-            5'b00110 : log_offset <= ACC_BW'b0111111111110000;
-            5'b00111 : log_offset <= ACC_BW'b0111111111110000;
-            5'b01000 : log_offset <= ACC_BW'b0000000101100000;
-            5'b01001 : log_offset <= ACC_BW'b1010000000111110;
-            5'b01010 : log_offset <= ACC_BW'b0010000101000100;
-            5'b01011 : log_offset <= ACC_BW'b1110001101110100;
-            5'b01100 : log_offset <= ACC_BW'b0000100101100000;
-            5'b01101 : log_offset <= ACC_BW'b1011001011111100;
-            5'b01110 : log_offset <= ACC_BW'b0111111111111100;
-            5'b01111 : log_offset <= ACC_BW'b1111111111111000;
-            5'b10000 : log_offset <= ACC_BW'b0001000000000000;
-            5'b10001 : log_offset <= ACC_BW'b0010101110000000;
-            5'b10010 : log_offset <= ACC_BW'b0111011001001000;
-            5'b10011 : log_offset <= ACC_BW'b0111111111110010;
-            5'b10100 : log_offset <= ACC_BW'b0111111111010000;
-            5'b10101 : log_offset <= ACC_BW'b0111110011110000;
-            5'b10110 : log_offset <= ACC_BW'b0111111111110000;
-            5'b10111 : log_offset <= ACC_BW'b0111111111110000;
-            5'b11000 : log_offset <= ACC_BW'b0000000101100000;
-            5'b11001 : log_offset <= ACC_BW'b1010000000111110;
-            5'b11010 : log_offset <= ACC_BW'b0010000101000100;
-            5'b11011 : log_offset <= ACC_BW'b1110001101110100;
-            5'b11100 : log_offset <= ACC_BW'b0000100101100000;
-            5'b11101 : log_offset <= ACC_BW'b1011001011111100;
-            5'b11110 : log_offset <= ACC_BW'b0111111111111100;
-            5'b11111 : log_offset <= ACC_BW'b1111111111111000;
-            default : log_offset <= ACC_BW'b0000000000000000;
+            5'b00000 : log_offset <= 'b0001000000000000;
+            5'b00001 : log_offset <= 'b0010101110000000;
+            5'b00010 : log_offset <= 'b0111011001000000;
+            5'b00011 : log_offset <= 'b0111111111110000;
+            5'b00100 : log_offset <= 'b0111111111110000;
+            5'b00101 : log_offset <= 'b0111111111110000;
+            5'b00110 : log_offset <= 'b0111111111110000;
+            5'b00111 : log_offset <= 'b0111111111110000;
+            5'b01000 : log_offset <= 'b0000000101100000;
+            5'b01001 : log_offset <= 'b1010000000111110;
+            5'b01010 : log_offset <= 'b0010000101000100;
+            5'b01011 : log_offset <= 'b1110001101110100;
+            5'b01100 : log_offset <= 'b0000100101100000;
+            5'b01101 : log_offset <= 'b1011001011111100;
+            5'b01110 : log_offset <= 'b0111111111111100;
+            5'b01111 : log_offset <= 'b1111111111111000;
+            5'b10000 : log_offset <= 'b0001000000000000;
+            5'b10001 : log_offset <= 'b0010101110000000;
+            5'b10010 : log_offset <= 'b0111011001001000;
+            5'b10011 : log_offset <= 'b0111111111110010;
+            5'b10100 : log_offset <= 'b0111111111010000;
+            5'b10101 : log_offset <= 'b0111110011110000;
+            5'b10110 : log_offset <= 'b0111111111110000;
+            5'b10111 : log_offset <= 'b0111111111110000;
+            5'b11000 : log_offset <= 'b0000000101100000;
+            5'b11001 : log_offset <= 'b1010000000111110;
+            5'b11010 : log_offset <= 'b0010000101000100;
+            5'b11011 : log_offset <= 'b1110001101110100;
+            5'b11100 : log_offset <= 'b0000100101100000;
+            5'b11101 : log_offset <= 'b1011001011111100;
+            5'b11110 : log_offset <= 'b0111111111111100;
+            5'b11111 : log_offset <= 'b1111111111111000;
+            default : log_offset <= 'b0000000000000000;
         endcase
     end
 
